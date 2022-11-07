668ad638614d82d454f5944ab7ed97d3e4301410
Alejandro Miron <113551832+amironromer@users.noreply.github.com>
2022-11-07 20:38:10 +0000

    iterations

---
+++ main.py
@@ -0,0 +1,25 @@
+from methods_help import method_help
+from functions import function
+from returnStuff import returnS
+
+method_help()
+function()
+returnS()
+sticks= ["-", "--","---","----","-----"]
+
+
+from function_interactions import mix
+my_mix=mix(sticks)
+
+from function_interactions import try_your_luck
+try1=try_your_luck()
+print(try1)
+
+
+from function_interactions import verify_my_try
+
+
+
+
+
+
